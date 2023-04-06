09988b42bcacc3a4c5ed3327c92eac4da97258a5
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-04-07 01:05:30 +0530

    Add files via upload

---
+++ src/Components/Contactus.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Contactus()
+{
+
+    return(
+
+        <React.Fragment>
+             <div className='container'>
+             <h5>Contact us</h5>
+                </div>
+           
+        </React.Fragment>
+    );
+}
+
+export default Contactus;
---
+++ src/Components/Pagenotfound.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Pagenotfound()
+{
+    return(
+        <React.Fragment>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-2">
+                        <h5 className="text-danger"> Page Not Found</h5>
+                        </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+export default Pagenotfound;
